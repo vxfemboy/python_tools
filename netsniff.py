a1653c63b688e050bc2bf4d4b701001a7aa81977
Ø <79362520+legitnull@users.noreply.github.com>
2021-05-01 21:13:53 -0600

    Add files via upload

---
+++ netsniff.py
@@ -0,0 +1,23 @@
+#NETWORK SNIFFER
+import scapy.all as scapy
+import argparse
+from scapy.layers import http
+
+def args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i','--interface','--iface', dest='iface', help='Interface to Change the MAC')
+    (val) =  parser.parse_args()
+    if not val.iface:
+        parser.error('INVALID ARGS, USE --help')
+    return val
+def sniff(iface):
+    scapy.sniff(iface=iface, store=False, prn=sniffpacc)
+
+def sniffpacc(pacc):
+    if pacc.haslayer(http.HTTPRequest): #add https later 
+        print(pacc)
+
+val = args()
+sniff(val.iface)
+#except OSError: #detect if iface is correct
+#    print('INTERFACE NOT FOUND!')
